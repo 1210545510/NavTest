d3ec06a708311130bb7d8b1b1ff050710f9e1c7a
Shipeng <sun.ship@husky.neu.edu>
2017-05-14 22:56:24 -0400

    NavTest

---
+++ css/g.alicdn.com/tb-mod/world-footer/0.1.4/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by shipeng on 5/14/2017.
+ */
+KISSY.add("tb-mod/world-footer/0.1.4/index",["node","kg/world-footer/0.1.1/"],function(n,o){function t(){this.init.apply(this,arguments)}var i=o("node"),e=i.all,r=o("kg/world-footer/0.1.1/");return new r({container:e(".footer-container"),hasTips:!0,hasLinks:!0}),t.prototype={init:function(n){},loadData:function(){},bindEvent:function(){}},t});
